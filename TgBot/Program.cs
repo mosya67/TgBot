5bb347b1132d3eef34303f7f76667efe55d78ac3
admin <admin@DESKTOP-QUE1O1K>
2024-03-31 22:27:55 +0300

    ee

--- TgBot/Program.cs
+++ TgBot/Program.cs
@@ -37,7 +37,6 @@ namespace TelegramBot
         static void Main(string[] args)
         {
             ComponentInitialization();
-            ScopedComponentInitialization();
 
             var proxy = new WebProxy
             {
